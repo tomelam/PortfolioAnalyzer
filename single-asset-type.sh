6ee254b324021527ed8670c9b581aec9714b5093
Tom Elam <tomelam@gmail.com>
2025-03-24 19:09:51 +0530

    Added a shell script to analyze each single-asset-type portfolio.

---
+++ single-asset-type.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python main.py port/port-ppf.toml
+python main.py port/port-scss.toml
+python main.py port/port-1.toml
+python main.py port/port-sgb.toml
+python main.py port/port-gold.toml
+python main.py port/port-rec-bond.toml
